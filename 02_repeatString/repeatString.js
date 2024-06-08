fbab71d36c41a858aadba1c0692cf7a6503c4c51
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-06-08 22:16:57 +0700

    feat: Implement repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
 
+  if (times < 0) {
+    return "ERROR";
+  }
+
+  let repeatedString = "";
+
+  for (let i = 0; i < times; i += 1) {
+    repeatedString += word;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
