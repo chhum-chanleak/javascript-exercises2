3ac8a6dd9718974c062e294cf2ff3870d68f6cbe
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-06-09 10:47:57 +0700

    feat: Implement leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  console.assert(typeof(year) === 'number', "Year must a be a number.");
 
+  return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0))
 };
 
 // Do not edit below this line
