32eafe9886cc129690f113a9296f2ca39f538a62
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-06-09 09:32:54 +0700

    feat: Implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(init, termi) {
 
+  console.log(typeof(init), typeof(termi));
+  if ((init < 0 || termi < 0) ||
+  (typeof(init) !== 'number' || typeof(termi) !== 'number')) {
+    return "ERROR";
+  }
+
+  let reversedInit = termi;
+  let reversedTermi = init;
+
+  if (init > termi) {
+    init = reversedInit;
+    termi = reversedTermi;
+  }
+
+  let diff = 1;
+  const sumTimes = termi - init;
+  let totalSum = init;
+  let nextNum;
+
+  for (let i = 0; i < sumTimes; i += 1) {
+    nextNum = init + diff;
+    diff += 1;
+    totalSum += nextNum;
+  }
+
+  return totalSum;
 };
 
 // Do not edit below this line
