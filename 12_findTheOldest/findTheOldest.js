b1ca113fc04bf6341df9a9234cb659482d8f4dff
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-07-20 16:43:56 +0700

    feat: Implement getMax(), which takes in an array of numbers and returns the greatest number

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const getMax = (arr) => {
+  let max = arr[0];
 
+  for (let i = 0; i < arr.length; i += 1) {
+    if (max > arr[i + 1]) {
+      max = max;
+    } else if (max < arr[i + 1]) {
+      max = arr[i + 1];
+    }
+  }
+  return max;
+};
+
+const findTheOldest = function(arr) {
+  let oldest;
+
+  for (let i = 0; i < arr.length; i += 1) {
+
+  }
 };
 
 // Do not edit below this line
