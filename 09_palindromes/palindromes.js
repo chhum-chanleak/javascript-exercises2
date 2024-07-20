eedac9309fb67b79422bbccf0ba7bd33ac94f91e
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-07-20 15:54:39 +0700

    feat: Implement palindromes(), which takes a string and returns true when the is is spelled the same after being reversed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const charArr = filterAlphabets(str).split('');
+  const reversedStr = [];
 
+  for (let i = 0; i < charArr.length; i += 1) {
+    reversedStr.push(charArr.pop());
+    i -= 1;
+  }
+  console.log(filterAlphabets(str));
+  return filterAlphabets(str) === reversedStr.join('');
+};
+
+const filterAlphabets = (str) => { // background-color
+  const alphabets = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let convertedStr = str.toLowerCase();
+  let newStr = []; 
+
+  for (let i = 0; i < convertedStr.length; i += 1) {
+    if (alphabets.includes(convertedStr[i])) {
+      newStr.push(convertedStr[i]);
+    }
+  }
+  return newStr.join('');
 };
 
 // Do not edit below this line
