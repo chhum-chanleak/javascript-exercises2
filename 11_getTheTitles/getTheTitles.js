2db92b1c370c35c4995aad89c2aabeb9cf85d13f
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-07-20 16:37:03 +0700

    feat: Implement getTheTitles(), which takes in an array of objects and returns an array of titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,21 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
 
+const getTheTitles = function(arr) {
+  let books = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    books.push(arr[i].title);
+  }
+  return books;
 };
 
 // Do not edit below this line
