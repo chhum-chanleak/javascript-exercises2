120a49a91d8bab0d3894f63b46f76c4fae79a6ea
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-07-20 17:15:01 +0700

    feat: Implement findProperty(), which takes in an object and and property name and returns true when the property name does not exist. Returns false otherwise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,3 +1,37 @@
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+const people2 = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
 const getMax = (arr) => {
   let max = arr[0];
 
@@ -11,13 +45,18 @@ const getMax = (arr) => {
   return max;
 };
 
-const findTheOldest = function(arr) {
-  let oldest;
+// Find a property of an object. Return false if there is no such property
+const findProperty = (obj, propName) => {
 
-  for (let i = 0; i < arr.length; i += 1) {
-
+  for (let prop in obj) {
+    if (prop === propName) {
+      return true;
+    }
   }
+  return false
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
