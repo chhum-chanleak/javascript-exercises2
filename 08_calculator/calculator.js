b0239da5a9b12882b4810d9d57d33a9f0ad6abc1
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-07-20 15:21:26 +0700

    feat: Implement multiple functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,61 @@
-const add = function() {
-	
+const add = function(num1 = 1, num2 = 2) {
+  return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1 = 1, num2 = 2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    result += arr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
+const sumV2 = (arr) => {
+  const result = arr.reduce((acc, cur) => acc + cur, 0);
 
+  return result;
 };
 
-const power = function() {
-	
+const multiply = function(arr) {
+  let result = 1;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    result *= arr[i];
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const multiplyV2 = (arr) => {
+  const result = arr.reduce((acc, cur) => acc * cur, 1);
+  
+  return result;
+};
+
+const power = function(base, pow) {
+	return base ** pow;
+};
+
+const powerV2 = (base, pow) => {
+  return Math.pow(base, pow);
+};
+
+const factorial = function(terms) {
+	let result = 1;
+
+  if (parseInt(terms) === 0 || parseInt(terms) === 1) {
+    return 1;
+  }
+
+  for (let i = 0; i < terms; i += 1) {
+    result *= parseInt(terms) - i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
