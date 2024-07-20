650c6cf8cdb85cd7c6799e635ea0a064fa75aee5
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-07-20 16:09:34 +0700

    feat: Implement fibonacci(), which takes in the number of terms and returns the last term

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(terms) {
+  if (parseInt(terms) === 1 || parseInt(terms) === 2) {
+    return 1;
+  } else if (parseInt(terms) === 0) {
+    return 0;
+  } else if (parseInt(terms) < 0) {
+    return 'OOPS';
+  }
 
+  let fibSequence = [1, 1];
+  let nextTerm = 0;
+
+  for (let i = 2; i < terms; i += 1) {
+    nextTerm = fibSequence[i - 1] + fibSequence[i - 2];
+    fibSequence.push(nextTerm);
+  }
+  return fibSequence[parseInt(terms) - 1];
 };
 
 // Do not edit below this line
